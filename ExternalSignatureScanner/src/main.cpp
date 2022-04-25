935535f6626214d9df16fb1d8609671e20b9c9bd
vnescape <vnescape@protonmail.com>
2022-04-25 19:23:24 +0200

    Add parseCommandLineArguments()

--- ExternalSignatureScanner/src/main.cpp
+++ ExternalSignatureScanner/src/main.cpp
@@ -1,23 +1,48 @@
-
 #include <iostream>
 #include <Windows.h>
 
+int getProcessIdByName(std::string proccesName)
+{
+	// TODO
+	return 0;
+}
+
+void parseCommandLineArguments(int& procId, std::string& signature)
+{
+	int argcW;
+	LPWSTR* argvW;
+	std::string procName;
+
+	// get commannd line arguments as wide chars 
+	argvW = CommandLineToArgvW(GetCommandLineW(), &argcW);
+	procName = *argvW[1];
+	signature = *argvW[2];
+
+	// if process name is used instead of providing process id
+	if (procName.find(".exe") != std::string::npos)
+	{
+		procId = getProcessIdByName(procName);
+	}
+	else
+	{
+		procId = std::stio(procName); 
+	}
+
+}
 
 
 int main(int argc, char **argv)
 {
 	SYSTEM_INFO lpSystemInfo;
-	int argcW;
-	LPWSTR* argvW;
 
 	// check for arguments
 	if (argc < 3) {
 		std::cout << "Usage: .\\ExternalSignatureScanner.exe <processID / processName> <signature>" << std::endl;
 		return 1;
 	}
-
-	// get commannd line arguments as wide chars 
-	argvW = CommandLineToArgvW(GetCommandLineW(), &argcW);
+	int procId;
+	std::string signature;
+	parseCommandLineArguments(procId, signature);
 
 	// retrieve system informations
 	GetSystemInfo(&lpSystemInfo);
