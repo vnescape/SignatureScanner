c651e5bf668c32f8d5ff3156b8965400169e5b5f
vnescape <vnescape@protonmail.com>
2022-04-25 18:54:26 +0200

    Print usage

--- ExternalSignatureScanner/src/main.cpp
+++ ExternalSignatureScanner/src/main.cpp
@@ -2,12 +2,24 @@
 #include <iostream>
 #include <Windows.h>
 
-int main() {
+void printUsage()
+{
+	std::cout << "Usage:"
+
+}
+
+int main(int argc, char **argv)
+{
+	if (argc < 3) {
+		std::cout << "Usage: .\\ExternalSignatureScanner.exe <processID / processName> <signature>" << std::endl;
+		return 1;
+	}
 
 	SYSTEM_INFO lpSystemInfo;
 
 	// retrieve system informations
 	GetSystemInfo(&lpSystemInfo);
 	std::cout << lpSystemInfo.dwNumberOfProcessors << std::endl;
+
 	return 0;
 }
