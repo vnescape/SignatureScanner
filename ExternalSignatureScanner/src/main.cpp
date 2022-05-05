545fbbd4a5af672562b72fbc562eb7cb6d027663
vnescape <vnescape@protonmail.com>
2022-05-05 10:17:50 +0200

    Seperate code into own function scanFullMemeory()

--- ExternalSignatureScanner/src/main.cpp
+++ ExternalSignatureScanner/src/main.cpp
@@ -3,6 +3,38 @@
 #include <Windows.h>
 #include <tlhelp32.h>
 
+void scanFullMemory(DWORD procId)
+{
+
+	HANDLE targetProcess = OpenProcess(PROCESS_QUERY_INFORMATION, true, procId);
+	HANDLE moduleSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, procId);
+
+	LPCVOID addr = NULL;
+	MEMORY_BASIC_INFORMATION memInfo;
+
+	while (VirtualQueryEx(targetProcess, addr, &memInfo, sizeof(memInfo)))
+	{
+		if (memInfo.State == MEM_COMMIT && memInfo.Protect != PAGE_NOACCESS)
+		{
+			std::cout << " [MEM_COMMIT && !PAGE_NOACCESS] Address: " << addr << std::endl;
+			for (LPCVOID i = addr; i < (char*)addr + memInfo.RegionSize; i = (char*)i + 1) // increment by 1 Byte
+			{
+				std::cout << "    |- " << i << "\r"; // moving the cursor back to the start
+			}
+		}
+		else
+		{
+			//std::cout << " Address: " << addr << '\r'; // reset line by moving the cursor to the start of the line
+			std::cout << " Address: " << addr << std::endl;
+		}
+		// cast to char* so bytes will be added
+		addr = (char*)addr + memInfo.RegionSize;
+	}
+
+	CloseHandle(targetProcess);
+
+}
+
 void getProcessNameOrId(DWORD& outProcId, std::wstring& outProcName)
 {
 	PROCESSENTRY32 entry;
@@ -103,32 +135,6 @@ int main(int argc, char** argv)
 	MODULEENTRY32 me32;
 	me32.dwSize = sizeof(MODULEENTRY32);
 
-	HANDLE targetProcess = OpenProcess(PROCESS_QUERY_INFORMATION, true, procId);
-	HANDLE moduleSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, procId);
-
-	LPCVOID addr = NULL;
-	MEMORY_BASIC_INFORMATION memInfo;
-
-
-	while (VirtualQueryEx(targetProcess, addr, &memInfo, sizeof(memInfo)))
-	{
-		if (memInfo.State == MEM_COMMIT && memInfo.Protect != PAGE_NOACCESS)
-		{
-			std::cout << " [MEM_COMMIT && !PAGE_NOACCESS] Address: " << addr << std::endl;
-			for (LPCVOID i = addr; i < (char*)addr + memInfo.RegionSize; i = (char*)i + 1) // increment by 1 Byte
-			{
-				std::cout << "    |- " << i << "\r"; // moving the cursor back to the start
-			}
-		}
-		else
-		{
-			//std::cout << " Address: " << addr << '\r'; // reset line by moving the cursor to the start of the line
-			std::cout << " Address: " << addr << std::endl;
-		}
-		// cast to char* so bytes will be added
-		addr = (char*)addr + memInfo.RegionSize;
-	}
-	
-	CloseHandle(targetProcess);
+	scanFullMemory(procId);
 	return 0;
 }
